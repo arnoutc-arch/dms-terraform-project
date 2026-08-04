26b4d4a296b0eba45448aa1c99c9f8c718843681
arnoutc-arch <arnout.cator1@digital.homeoffice.gov.uk>
2026-08-04 23:53:21 +0100

    adding target security group

--- target-account/aurora.tf
+++ target-account/aurora.tf
@@ -45,15 +45,3 @@ resource "aws_db_subnet_group" "target" {
   name = "target-subnet-group"
   subnet_ids = var.private_subnet_ids
 }
-
-resource "aws_security_group" "target_db" {
-  name   = "target-db-sg"
-  vpc_id = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
