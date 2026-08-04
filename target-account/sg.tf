6ad8a2294a52594b197c8bf9c737afc7ca7722be
arnoutc-arch <arnout.cator1@digital.homeoffice.gov.uk>
2026-08-04 23:52:37 +0100

    adding target security group

--- target-account/sg.tf
+++ target-account/sg.tf
@@ -1,3 +1,15 @@
+resource "aws_security_group" "target_db" {
+  name   = "target-db-sg"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group_rule" "target_from_dms" {
   type                     = "ingress"
   protocol                 = "tcp"
